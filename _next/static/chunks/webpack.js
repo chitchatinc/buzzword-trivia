00576b0c1d46be8286e564f84ea37dfbea50dc52
Penny Shen <penny@medium.com>
2021-01-04 01:42:11 -0800

    fix link try 2

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "69998fcf894524985510";
+/******/ 	var hotCurrentHash = "261015be16499df45a52";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
