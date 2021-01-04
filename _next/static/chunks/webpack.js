8652078f771c0028504cde1abf0deabfe87e6e2a
Penny Shen <penny@medium.com>
2021-01-04 02:05:01 -0800

    trying to fix fouc

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "261015be16499df45a52";
+/******/ 	var hotCurrentHash = "22559abbfbda1bf310b0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
